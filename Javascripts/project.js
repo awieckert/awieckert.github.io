f5c84db3804a93d8ee0ca322023164d0bf959300
Adam Wieckert <wiec1369@gmail.com>
2018-03-30 17:11:28 -0500

    Added the project cards to a JSON file and got them printing to the page. Need help with bootstrap card styling.

--- Javascripts/project.js
+++ Javascripts/project.js
@@ -1,50 +1,5 @@
 console.log("YAY!");
 
-var projects = [
-    {
-        id: "Project 1",
-        title: "Super Cool Title 1",
-        imgURL: "img/bug-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-    {
-        id: "Project 2",
-        title: "Super Cool Title 2",
-        imgURL: "img/not-funny-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-    {
-        id: "Project 3",
-        title: "Super Cool Title 3",
-        imgURL: "img/airplane-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-    {
-        id: "Project 4",
-        title: "Super Cool Title 4",
-        imgURL: "img/jedi-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-    {
-        id: "Project 5",
-        title: "Super Cool Title 5",
-        imgURL: "img/must-ask-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-    {
-        id: "Project 6",
-        title: "Honestly, Not That Great of a Title",
-        imgURL: "img/thermo-cat.jpg",
-        description: "This might be the most awesome project ever created, no seriously its really cool! Okay maybe it's not that great but you should still check it out.",
-        gitHubURL: "https://awieckert.github.io/"
-    },
-];
-
 function writeToDom (stringToPrint, divID){
     var printDiv = document.getElementById(divID);
     printDiv.innerHTML = stringToPrint;
@@ -57,7 +12,7 @@ function createProjectCards (arrayOfObjects) {
     for(var i = 0; i < arrayOfObjects.length; i++){
         currentObject = arrayOfObjects[i];
         stringToPrint += "<div class='col-md-4'>";
-        stringToPrint += "<div class='card' style='width: 18rem;'>";
+        stringToPrint += "<div class='card project-card' style='width: 18rem;'>";
         stringToPrint += "<img class='card-img-top' src=" + currentObject.imgURL + " alt='Cat Image'>";
         stringToPrint += "<div class='card-body'>";
         stringToPrint += "<h4 class='card-title'>" + currentObject.title + "</h4>";
@@ -70,4 +25,21 @@ function createProjectCards (arrayOfObjects) {
     writeToDom(stringToPrint, "project-container");
 }
 
-createProjectCards(projects);
+function onLoad () {
+    const data = JSON.parse(this.responseText);
+    createProjectCards(data.projects);
+}
+
+function WTF () {
+    console.log("OMG ERROR!!!");
+}
+
+const startApplication = () => {
+    const myRequest = new XMLHttpRequest;
+    myRequest.addEventListener('load', onLoad);
+    myRequest.addEventListener('error', WTF);
+    myRequest.open("GET", "../db/projects.json");
+    myRequest.send();
+}
+
+startApplication();
