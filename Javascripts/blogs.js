be4dde9c600d4e537960e28747f3723e9aef76ed
Adam Wieckert <wiec1369@gmail.com>
2018-06-05 21:31:18 -0500

    WIP

--- Javascripts/blogs.js
+++ Javascripts/blogs.js
@@ -21,7 +21,7 @@ const createBlogCards = (arrayOfObjects) => {
     let stringToPrint = "";
     for(let i = 0; i < arrayOfObjects.length; i++){
         currentObject = arrayOfObjects[i];
-        stringToPrint += "<div class='col-md-11 mb-3' id='Blog " + i + "'>";
+        stringToPrint += `<div data-blogId="${currentObject.id}" class='col-md-11 mb-3' id='Blog " + i + "'>`;
         stringToPrint += "<div class='card'>";
         stringToPrint += "<div class='card-body'>";
         stringToPrint += `<h4 class='card-title'>${currentObject.title}</h4>`;
@@ -37,14 +37,14 @@ const createBlogCards = (arrayOfObjects) => {
 };
 
 
-function onLoad () {
-    const data = JSON.parse(this.responseText);
-    createBlogCards(data.blogs);
-};
+// function onLoad () {
+//     const data = JSON.parse(this.responseText);
+//     createBlogCards(data.blogs);
+// };
 
-function onFail () {
-    console.log("SHIT! It Broke");
-};
+// function onFail () {
+//     console.log("SHIT! It Broke");
+// };
 
 const getApiKeys = () => {
   return new Promise ((resolve, reject) => {
@@ -65,13 +65,43 @@ const callGetApiKeys = () => {
   });
 };
 
-const startApplication = () => {
-    let myRequest = new XMLHttpRequest;
-    myRequest.addEventListener("load", onLoad);
-    myRequest.addEventListener("error", onFail);
-    myRequest.open("GET", "../db/blogs.json");
-    myRequest.send();
+const getAllBlogs = () => {
+  return new Promise ((resolve, reject) => {
+    const allBlogs = [];
+    $.ajax(`${firebaseConfig.databaseURL}/blogs/blogs.json`).done((allFirebaseBlogs) => {
+      if (allFirebaseBlogs != null) {
+        Object.keys(allFirebaseBlogs).forEach((key) => {
+          allFirebaseBlogs[key].id = key;
+          allBlogs.push(allFirebaseBlogs[key]);
+        });
+      }
+      resolve(allBlogs);
+    }).fail((err) => {
+      reject(err);
+    });
+  });
+};
 
+const printTheBlogs = () => {
+  getAllBlogs().then((blogsArray) => {
+    createBlogCards(blogsArray);
+  }).catch((err) => {
+    console.log('The Blogs Could not print: ', err);
+  });
+};
+
+const addBlogEvent = () => {
+  $('#navitem5').on('click', printTheBlogs);
+};
+
+const startApplication = () => {
+    // let myRequest = new XMLHttpRequest;
+    // myRequest.addEventListener("load", onLoad);
+    // myRequest.addEventListener("error", onFail);
+    // myRequest.open("GET", "../db/blogs.json");
+    // myRequest.send();
+
+    addBlogEvent();
     callGetApiKeys();
 };
 
