767df05132397b5703d23297111c4025a4c7be82
Adam Wieckert <wiec1369@gmail.com>
2018-06-10 21:19:21 -0500

    Added Bootstrap stuff

--- Javascripts/blogs.js
+++ Javascripts/blogs.js
@@ -1,5 +1,3 @@
-console.log("YAY!");
-
 let firebaseConfig = {};
 
 const setFirebaseConfig = (datums) => {
@@ -11,40 +9,29 @@ const getFirebaseConfig = () => {
 };
 
 const writeToDom = (stringToPrint, divID) => {
-    let printDiv = document.getElementById(divID);
-    printDiv.innerHTML = stringToPrint;
+  const printDiv = document.getElementById(divID);
+  printDiv.innerHTML = stringToPrint;
 };
-
 
 const createBlogCards = (arrayOfObjects) => {
-    let currentObject;
-    let stringToPrint = "";
-    for(let i = 0; i < arrayOfObjects.length; i++){
-        currentObject = arrayOfObjects[i];
-        stringToPrint += `<div data-blogId="${currentObject.id}" class='col-md-11 mb-3' id='Blog " + i + "'>`;
-        stringToPrint += "<div class='card'>";
-        stringToPrint += "<div class='card-body'>";
-        stringToPrint += `<h4 class='card-title'>${currentObject.title}</h4>`;
-        stringToPrint += `<h5 class='card-subtitle mb-2 text-muted'>${currentObject.date}</h5>`;
-        stringToPrint += `<h4>Strengths:</h4><p class='card-text'>${currentObject.strengths}</p><h4>Weaknesses:</h4><p class='card-text'>${currentObject.weaknesses}</p>`;
-        stringToPrint += "<a href='#Blog " + (i - 1) + "' class='btn btn-primary'>Go to previous</a>";
-        stringToPrint += "<a href='#Blog " + (i + 1) + "' class='btn btn-primary ml-3'>Go to next</a>";
-        stringToPrint += "</div>";
-        stringToPrint += "</div>";
-        stringToPrint += "</div>";
-    }
-    writeToDom(stringToPrint, "blogs-container");
+  let currentObject;
+  let stringToPrint = '';
+  for (let i = 0; i < arrayOfObjects.length; i++) {
+    currentObject = arrayOfObjects[i];
+    stringToPrint += `<div data-blogId='${currentObject.id}' class='col-md-11 mb-3' id='Blog${i}'>`;
+    stringToPrint += `<div class='card'>`;
+    stringToPrint += `<div class='card-body'>`;
+    stringToPrint += `<h4 class='card-title'>${currentObject.title}</h4>`;
+    stringToPrint += `<h5 class='card-subtitle mb-2 text-muted'>${currentObject.date}</h5>`;
+    stringToPrint += `<h4>Strengths:</h4><p class='card-text'>${currentObject.strengths}</p><h4>Weaknesses:</h4><p class='card-text'>${currentObject.weaknesses}</p>`;
+    stringToPrint += `<a href='#Blog${(i - 1)}' class='btn btn-primary'>Go to previous</a>`;
+    stringToPrint += `<a href='#Blog${(i + 1)}' class='btn btn-primary ml-3'>Go to next</a>`;
+    stringToPrint += `</div>`;
+    stringToPrint += `</div>`;
+    stringToPrint += `</div>`;
+  }
+  writeToDom(stringToPrint, 'blogs-container');
 };
-
-
-// function onLoad () {
-//     const data = JSON.parse(this.responseText);
-//     createBlogCards(data.blogs);
-// };
-
-// function onFail () {
-//     console.log("SHIT! It Broke");
-// };
 
 const getApiKeys = () => {
   return new Promise ((resolve, reject) => {
@@ -95,14 +82,8 @@ const addBlogEvent = () => {
 };
 
 const startApplication = () => {
-    // let myRequest = new XMLHttpRequest;
-    // myRequest.addEventListener("load", onLoad);
-    // myRequest.addEventListener("error", onFail);
-    // myRequest.open("GET", "../db/blogs.json");
-    // myRequest.send();
-
-    addBlogEvent();
-    callGetApiKeys();
+  addBlogEvent();
+  callGetApiKeys();
 };
 
 startApplication();
