44c75b6b5b763c4a060d6d9c43523be1f0920c17
Adam Wieckert <wiec1369@gmail.com>
2018-06-12 18:51:52 -0500

    WIP

--- javascripts/firebaseProjects.js
+++ javascripts/firebaseProjects.js
@@ -2,7 +2,7 @@ const getFirebaseProjects = (firebaseConfig) => {
   return new Promise ((resolve, reject) => {
     $.ajax({
       method: 'GET',
-      url: `${firebaseConfig.data}`
+      url: `${firebaseConfig.databaseURL}/`
     }).done((projects) => {
       resolve(projects);
     }).fail((err) => {
