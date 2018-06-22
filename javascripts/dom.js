9a58c349cccee1b692576612d9d5284a6361ee5d
Adam Wieckert <wiec1369@gmail.com>
2018-06-22 09:32:02 -0500

    Working on hiding blogs

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -20,13 +20,40 @@ const printProjects = (projectsArray) => {
 };
 
 const printBlogs = (blogsArray) => {
-  let stringToPrint = '';
+  let blogsToPrint = '';
+  let paginationToPrint = '';
   const arrayLength = blogsArray.length;
-  blogsArray.forEach((blog) => {
-
+  const paginationLength = Math.round(arrayLength / 3);
+  blogsArray.forEach((blog, i) => {
+    if (i >= 3) {
+      blogsToPrint += `<div href="#" class="list-group-item list-group-item-action flex-column align-items-start active hide">`;
+    } else {
+      blogsToPrint += `<div href="#" class="list-group-item list-group-item-action flex-column align-items-start active">`;
+    }
+    blogsToPrint +=  `<div class="d-flex w-100 justify-content-between">`;
+    blogsToPrint +=    `<h5 class="mb-1">${blog.title}</h5>`;
+    blogsToPrint +=    `<small>3 days ago</small>`;
+    blogsToPrint +=  `</div>`;
+    blogsToPrint +=  `<p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit</p>`;
+    blogsToPrint +=  `<small>Donec id elit non mi porta.</small>`;
+    blogsToPrint +=  `</div>`;
   });
+
+  paginationToPrint += `<ul class="pagination">`;
+  paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">Previous</a></li>`;
+  for (let j = 1; j <= paginationLength; j++) {
+    paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">1</a></li>`;
+    paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">2</a></li>`;
+    paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">3</a></li>`;
+  }
+  paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
+  paginationToPrint +=  `</ul>`;
+
+  printToDom('#print-blogs', blogsToPrint);
+  printToDom('#print-pagination', paginationToPrint);
 };
 
 module.exports = {
   printProjects,
+  printBlogs,
 };
