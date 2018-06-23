01ea7c0775c0d31303cfad6f792b809413acca51
Adam Wieckert <wiec1369@gmail.com>
2018-06-23 14:46:37 -0500

    PAGINATIONgit statusgit status! YUS!

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -26,9 +26,9 @@ const printBlogs = (blogsArray) => {
   const paginationLength = Math.round(arrayLength / 3);
   blogsArray.forEach((blog, i) => {
     if (i >= 3) {
-      blogsToPrint += `<div id="Blog-${blog.id}" href="#" class="list-group-item list-group-item-action flex-column align-items-start active d-none">`;
+      blogsToPrint += `<div id="${blog.blogID}" href="#" class="list-group-item list-group-item-action flex-column align-items-start active d-none">`;
     } else {
-      blogsToPrint += `<div id="Blog-${blog.id}" href="#" class="list-group-item list-group-item-action flex-column align-items-start active">`;
+      blogsToPrint += `<div id="${blog.blogID}" href="#" class="list-group-item list-group-item-action flex-column align-items-start active">`;
     }
     blogsToPrint +=  `<div class="d-flex w-100 justify-content-between">`;
     blogsToPrint +=    `<h5 class="mb-1">${blog.title}</h5>`;
@@ -40,11 +40,11 @@ const printBlogs = (blogsArray) => {
   });
 
   paginationToPrint += `<ul class="pagination">`;
-  paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">Previous</a></li>`;
+  paginationToPrint +=  `<li class="page-item"><a id="previous" class="page-link blog-page">Previous</a></li>`;
   for (let j = 1; j <= paginationLength; j++) {
-    paginationToPrint +=  `<li class="page-item blog-page"><span class="page-link">${j}</span></li>`;
+    paginationToPrint +=  `<li class="page-item"><span id='page-${j}' class="page-link blog-page">${j}</span></li>`;
   }
-  paginationToPrint +=  `<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
+  paginationToPrint +=  `<li class="page-item"><a id='next' class="page-link blog-page">Next</a></li>`;
   paginationToPrint +=  `</ul>`;
 
   printToDom('#print-blogs', blogsToPrint);
--- javascripts/paginationEvents.js
+++ javascripts/paginationEvents.js
@@ -1,13 +1,38 @@
 let initialRange = [1, 2, 3,];
+let currentPage = 1;
 
 const addPaginationEvents = () => {
   $('body').on('click', '.blog-page', paginationStuff);
 };
 
 const paginationStuff = (e) => {
-  const pageNumberClicked = (($(e.target).text()) * 1);
-  const rangeMax = (pageNumberClicked * 3);
-  const rangeToInclude = [(rangeMax - 2), (rangeMax - 1), rangeMax,];
+  const paginationLength = (($('.blog-page').length) - 2);
+  let pageNumberClicked;
+  let rangeMax;
+  let rangeToInclude = [];
+
+  if (Number.isInteger($(e.target).text() * 1)) {
+    pageNumberClicked = (($(e.target).text()) * 1);
+    rangeMax = (pageNumberClicked * 3);
+    rangeToInclude = [(rangeMax - 2), (rangeMax - 1), rangeMax,];
+    currentPage = pageNumberClicked;
+  } else if (e.target.id === 'previous') {
+    if (currentPage > 1) {
+      currentPage = currentPage - 1;
+      rangeMax = (currentPage * 3);
+      rangeToInclude = [(rangeMax - 2), (rangeMax - 1), rangeMax,];
+    }
+  } else if (e.target.id === 'next') {
+    if (currentPage < paginationLength) {
+      currentPage = currentPage + 1;
+      rangeMax = (currentPage * 3);
+      rangeToInclude = [(rangeMax - 2), (rangeMax - 1), rangeMax,];
+    }
+  }
+
+  $('.blog-page').removeClass('current');
+  $(`#page-${currentPage}`).addClass('current');
+
   if (initialRange !== rangeToInclude) {
     $(`#Blog-${initialRange[0]}, #Blog-${initialRange[1]}, #Blog-${initialRange[2]}`).addClass('d-none');
     $(`#Blog-${rangeToInclude[0]}, #Blog-${rangeToInclude[1]}, #Blog-${rangeToInclude[2]}`).removeClass('d-none');
