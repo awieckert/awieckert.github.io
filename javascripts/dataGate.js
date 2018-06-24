f177a218b3e9eacd0898281ad784d89668e50c19
Adam Wieckert <wiec1369@gmail.com>
2018-06-24 12:31:57 -0500

    Sticky nav bar working. Background changing on scroll and changing back at the top. Need to find background image

--- javascripts/dataGate.js
+++ javascripts/dataGate.js
@@ -1,9 +1,11 @@
 const firebaseAPI = require('./firebaseAPI.js');
 const pagination = require('./paginationEvents.js');
+const events = require('./events.js');
 
 const initializer = () => {
   firebaseAPI.initializeFirebase();
   pagination.addPaginationEvents();
+  events.addScrollEvents();
 };
 
 module.exports = initializer;
